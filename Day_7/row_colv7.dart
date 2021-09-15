63a1c57d59bb6f0c4a72f56e7326b72f57d97e68
Md Riazul Hsan <md.riazul.hasan@g.bracu.ac.bd>
2021-09-15 11:28:31 +0600

    scene 7

---
+++ Day_7/row_colv7.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    row_col(),
+  );
+}
+
+class row_col extends StatelessWidget {
+  const row_col({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Row Col'),
+          backgroundColor: Colors.amber,
+        ),
+        body: Center(
+          child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Text("Button 2"),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.all(5.0),
+                      child: Text("Button 1"),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
